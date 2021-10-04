c97f1f04f938ea0573594e5fea49f53b72d62cb3
Amirkasra Amini <amirkasra.amini007@gmail.com>
2021-10-04 02:45:04 +0200

    Update pandh.py

--- Web scraper/bs4/bs4/pandh.py
+++ Web scraper/bs4/bs4/pandh.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 #url,req,soup
-url="http://forouzanfallah.ir/"
+url="https://www.varzesh3.com/"
 req=requests.get(url)
 soup=BeautifulSoup(req.text, "html.parser")
 # print(soup.find_all('h6'))
